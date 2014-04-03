6a951436eddea6962b46aaacc9c70050fae7bb0d
Yaroslav Herasym <yherasym@partnerpedia.com>
2014-04-03 14:11:02 -0700

    updating winIE.bat

--- Appzone_mvn/winIE.bat
+++ Appzone_mvn/winIE.bat
@@ -1,7 +1,1 @@
-mvn -version
-dir
-cd Appzone_mvn
-dir
-mvn dependency:copy-dependencies
-icacls ExePath\IEDriverServer.exe /grant Everyone:(GE)
-mvn -PLoginSuite test -Dbrowser=IE -DscreenResolution=ABC -Denvironment=https://my-qa.enterpriseappzone.com
+mvn -version && dir && mvn dependency:copy-dependencies && mvn compile && icacls ExePath\IEDriverServer.exe /grant Everyone:(GE) && mvn -PLoginSuite test -Dbrowser=IE -DscreenResolution=ABC -Denvironment=https://my-qa.enterpriseappzone.com
